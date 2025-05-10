e3e751be57f462ac9e66987a5bd273f9041a16f0
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2025-05-10 19:23:41 +0100

    Update server.js
    changed the production code

--- api/src/server.js
+++ api/src/server.js
@@ -12,12 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
 const __dirname = path.resolve(); // Get the current directory name
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../ui/dist"))); // Serve static files from the public directory
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../ui", "dist", "index.html")); // Serve index.html for all other routes
-  });
-}
+
 
 // Middleware
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable CORS for the client URL
@@ -26,6 +21,13 @@ app.use(express.json()); // Use express.json() middleware to parse JSON request 
 app.use("/api/auth", authRoutes); // Use the auth routes under the /api/auth path
 app.use("/api/users", userRoutes); // Use the user routes under the /api/users path
 app.use("/api/chat", chatRoutes); // Use the chat routes under the /api/chat path
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../ui/dist"))); // Serve static files from the public directory
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../ui", "dist", "index.html")); // Serve index.html for all other routes
+  });
+}
 
 // Start the server
 app.listen(PORT, () => {
